49fd9d548dc9729dde15e96a6b077d17c094a1bf
founder611 <founder@ecomonks.in>
2026-06-01 13:16:01 +0530

    fixed image files

--- karpooram/settings.py
+++ karpooram/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Myapp/static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
